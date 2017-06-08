7b2a3a62272589e3488bb323c7277b504638cfb9
gus <gnkarn@gmail.com>
2017-06-08 11:28:11 -0300

    server for heroku fix
    added server port form env variable instead of fixed

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var express = require('express'),
 var app = express(); // made express aplication
 var path = require('path');
 
-
 // viewed at http://localhost:8080
 
 // http.createServer(app).listen(8080,"192.168.0.16");
 // var server = http.Server(app);
-var server    = app.listen(3000);
+// use the following for Heroku
+var server= app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+//var server    = app.listen(3000);
+
 // llama la funcion socket con la funcion server como argumento
 // io es ahora organiza el intercambio de datos llamando a la funcion socket
 // import socket library
