0262901582ffaa78358a5fff7c82233299c1bb94
gus <gnkarn@gmail.com>
2017-06-08 12:38:29 -0300

    fix

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express'),
   http = require('http'),
   https = require('https');
 var app = express(); // made express aplication
+
+// todo lo que esta el directorio public , los usuarios lo ven directamente con la app
+app.use(express.static('public'));
+
 var path = require('path');
 const INDEX = path.join(__dirname, '/public/index.html');
 
@@ -30,8 +34,6 @@ var io = socket(server);
 //var server = app.listen(8080);
 var clientNum = 0;
 
-// todo lo que esta el directorio public , los usuarios lo ven directamente con la app
-app.use(express.static('public'));
 
 console.log('VIDEO socket server running');
 
