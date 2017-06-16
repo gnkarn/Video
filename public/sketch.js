158577476453f9ca2e0fb2cada1c0f02ea4f4de9
gus <gnkarn@gmail.com>
2017-06-16 17:11:43 -0300

    fixes ?

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,7 @@
 var video;
 
 var myJSON;
- var socket = io();
+var socket = io();
 
 var ledMatrixWidth = 20;
 var ledMatrixHeight = 24;
@@ -11,24 +11,24 @@ var myCanvasW = 800,
 var myCanvas;
 var h1;
 
-  var hScale = myCanvasW / ledMatrixWidth;
-  var vScale = myCanvasH / ledMatrixHeight;
+var hScale = myCanvasW / ledMatrixWidth;
+var vScale = myCanvasH / ledMatrixHeight;
 
 var lcolor = {
-    "r":0,
-    "g":0,
-    "b":0
+  "r": 0,
+  "g": 0,
+  "b": 0
 };
 
 // preparo el objeto para el mensaje
 var ledMatrix = [];
 
 // led element not needed so far
-function  LedElement(x,y,lcolor){
-    this.x=x;
-    this.y=y;
-     this.lcol.r = lcolor
-   }
+function LedElement(x, y, lcolor) {
+  this.x = x;
+  this.y = y;
+  this.lcolor = lcolor;
+}
 
 function setup() {
 
@@ -40,54 +40,58 @@ function setup() {
   //}
 
   myCanvas = createCanvas(myCanvasW, myCanvasH);
-  myCanvas.position(10, 400);
+  myCanvas.position(10, 300);
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(myCanvas.width / hScale, myCanvas.height / vScale); // sets the video dom element size
 
-// set up the matrix object and all elements
-for (var y = 0; y < video.height; y++) {
-  for (var x = 0; x < video.width; x++) {
-    var index = (video.width - x + 1 + (y * video.width)) * 4;
-    var r = x;
-    var g = y;
-    var b = 0;
-    console.log(r,g,b);
-    ledMatrix[y*ledMatrixWidth+x] = {"r":r,"g":g,"b":b};
+  // set up the matrix object and all elements
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width)) * 4;
+      lcolor.r = x;
+      lcolor.g = y;
+      lcolor.b = 0;
+      console.log(lcolor);
+      ledMatrix[y * ledMatrixWidth + x] = {
+        "r": lcolor.r,
+        "g": lcolor.g,
+        "b": lcolor.b
+      };
+    }
   }
-}
   console.log(ledMatrix);
 
-  socket.on('newclientconnect',function(data){
-			  document.footer = '';
-			  document.write(data.description);
-	  });
+  socket.on('newclientconnect', function(data) {
+    document.footer = '';
+    document.write(data.description);
+  });
 }
 
 function draw() {
-  background(51);
+  background(100);
 
   video.loadPixels();
   loadPixels();
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       var index = (video.width - x + 1 + (y * video.width)) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
+      lcolor.r = video.pixels[index + 0];
+      lcolor.g = video.pixels[index + 1];
+      lcolor.b = video.pixels[index + 2];
 
       var bright = video.pixels[index + 3]; // (r+g+b)/3;
 
       //var w = map(bright, 0, 255, 0, vScale);
       noStroke();
-      fill(r, g, b, bright);
+      fill(lcolor.r, lcolor.g, lcolor.b, bright);
       //rectMode(CENTER);
       rect(x * (hScale), y * (vScale), hScale, vScale);
-      ledMatrix[y*ledMatrixWidth+x] = {"r":r,"g":g,"b":b};
-
+      ledMatrix[y * ledMatrixWidth + x] = lcolor;
+      //ledMatrix[y*ledMatrixWidth+x] = {"r":r,"g":g,"b":b};
     }
   }
-   myJSON = JSON.stringify(ledMatrix);
-   socket.emit('msgMatrixAserver', myJSON);
+  myJSON = JSON.stringify(ledMatrix);
+  socket.emit('msgMatrixAserver', myJSON);
   //console.log(myJSON);
 }
